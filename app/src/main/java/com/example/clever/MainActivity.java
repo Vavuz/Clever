f754cfda9a855d902f4c8b7c28485e552835a7a1
Marco Vavassori <marcovava2001@gmail.com>
2022-03-07 15:23:51 +0000

    Add logo and 'add expense' button to main interface.

--- app/src/main/java/com/example/clever/MainActivity.java
+++ app/src/main/java/com/example/clever/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.clever;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // the title will be hidden
+        getSupportActionBar().hide(); // the title bar will be hidden
         setContentView(R.layout.activity_main);
+
+        // Button to add an expense to the list of expenses
+        Button addExpenseButton = findViewById(R.id.add_expense);
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "A new expense has been added!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
