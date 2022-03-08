188121228a2b306f847f3c28cbe2a55f5bd1a329
Marco Vavassori <marcovava2001@gmail.com>
2022-03-08 17:15:56 +0000

    Add list view to contain the subscriptions.

--- app/src/main/java/com/example/clever/MainActivity.java
+++ app/src/main/java/com/example/clever/MainActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.view.Window;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +27,28 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
+
+        // List view setting up
+        ListView attractionDetails = (ListView) findViewById (R.id.expenses_list);
+
+        //Navigation back is via phones back button
+        final String[] attractionDetailsArray = new String[] { "Netflix",
+                "Spotify",
+                "Gym",
+                "Vodafone",
+                "Sky",
+                "Google Drive",
+                "Amazon Prime",
+                "Prime Video",
+                "Deliveroo Plus"};
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                attractionDetailsArray);
+
+        attractionDetails.setAdapter(adapter);
+
 
         // Button to add an expense to the list of expenses
         Button addExpenseButton = findViewById(R.id.add_expense);
